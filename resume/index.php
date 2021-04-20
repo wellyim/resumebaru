b8c783506e76035fcbed179db992a74c3880a4ae
wellyim <321910011@student.machung.ac.id>
2021-04-20 18:35:40 +0700

    hapus skil

--- resume/index.php
+++ resume/index.php
@@ -95,40 +95,6 @@
             </div>
         </section>
         <hr class="m-0" />
-        <!-- Skills-->
-        <section class="resume-section" id="skills">
-            <div class="resume-section-content">
-                <h2 class="mb-5">Skills</h2>
-                <div class="subheading mb-3">Programming Languages & Tools</div>
-                <ul class="list-inline dev-icons">
-                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-less"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-npm"></i></li>
-                </ul>
-                <div class="subheading mb-3">Skill Lain-Lain</div>
-                <ul class="fa-ul mb-0">
-                <?php
-                        $displayskill=mysqli_query($koneksi,"select * from skill");
-                        while ($row=mysqli_fetch_assoc($displayskill))
-                        {
-                           
-                        ?>
-                    <li>
-                        <span class="fa-li"><i class="fas fa-check"></i></span>
-                        <?php
-                         echo $row['skillname'];
-                        }
-                        ?>
-                    </li>
-                </ul>
-            </div>
-        </section>
-        <hr class="m-0" />
-        <!-- Interests-->
         <section class="resume-section" id="interests">
             <div class="resume-section-content">
                 <h2 class="mb-5">Interests</h2>
